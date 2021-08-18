6aa42a23a70f40b3aac8e752bd3b9e10e2a8513d
valentina <valentinamarenda1@gmail.com>
2021-08-18 17:53:52 -0300

    segundo commit

--- desafio8.js
+++ desafio8.js
@@ -7,63 +7,97 @@ const fs = require('fs');
 const app = express()
 const port = 8080;
 
-const productos = [
-	{
-		"title": "Napkin - Beverage 1 Ply",
-		"price": 825,
-		"thumbnail": "https://robohash.org/voluptatumetcorrupti.png?size=50x50&set=set1",
-	},
-	{
-		"title": "Ketchup - Tomato",
-		"price": 980,
-		"thumbnail": "https://robohash.org/saepevoluptasnulla.png?size=50x50&set=set1",
-	},
-	{
-		"title": "Cumin - Whole",
-		"price": 499,
-		"thumbnail": "https://robohash.org/fugiatdoloresse.png?size=50x50&set=set1",
-	},
-	{
-		"title": "Napkin - Beverage 1 Ply",
-		"price": 825,
-		"thumbnail": "https://robohash.org/voluptatumetcorrupti.png?size=50x50&set=set1",
-	},
-	{
-		"title": "Ketchup - Tomato",
-		"price": 980,
-		"thumbnail": "https://robohash.org/saepevoluptasnulla.png?size=50x50&set=set1",
-	}
-]
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res)=>{
+
+	 res.send("prueba")
+	 console.log("prueba")
+
+    
+})
+//guardar productos con Id
+
+class Archivo {
+    constructor(nombre){
+        this.nombre = nombre
+    }
+     async guardarAsync (producto){
+         try{
+        const data = await fs.promises.readFile(this.nombre);
+        const json = JSON.parse(data.toString("utf-8"));
+        json.push({...producto,id:json.length});
+        try {
+            await fs.promises.writeFile(this.nombre,JSON.stringify(json, null,"\t"))
+            }catch(err){
+                throw new Error (err)
+            }
+         }catch(err){
+             console.log([])
+             try
+           {
+                await fs.promises.writeFile(this.nombre, JSON.stringify([{...producto, id:0}]))
+            }catch (err) {
+                throw new Error (err)
+            }
+             
+         }
+     }
+    
+    
+}
+
+
+let myfile = new Archivo ('./productosD8.txt');
+//(async () => {
+//   await myfile.guardarAsync({ "title": "Napkin - Beverage 1 Ply", "price": 825, "thumbnail": "https://robohash.org/voluptatumetcorrupti.png?size=50x50&set=set1" });
+//    await myfile.guardarAsync({ "title": "Ketchup - Tomato", "price": 980, "thumbnail": "https://robohash.org/saepevoluptasnulla.png?size=50x50&set=set1" })
+//    await myfile.guardarAsync({ "title": "Cumin - Whole", "price": 499, "thumbnail": "https://robohash.org/fugiatdoloresse.png?size=50x50&set=set1" })
+//  })();
 
  //a 
 app.get("/api/productos/listar", (req, res)=>{
-     const products = productos.forEach((element, index )=> {
-         console.log(element, index)
-         res.send(element)  
-     });
+	fs.promises
+	.readFile("./productosD8.txt")
+	.then(data=>data.toString('utf-8'))
+	.then(datos=>{ 
+	 res.send(datos)
+	 console.log(datos)
+ })
     
 })
 
 //b 
 app.get('/api/productos/listar/:id', (req, res) => {
-    if(!isNaN(req.params.id) && products.length > req.params.id){
-        const search = productos.filter( product => product .id === parseInt(req.params.id,10))
-        res.send(...search ) 
+	fs.promises
+	.readFile("./productosD8.txt")
+	.then(data=>data.toString('utf-8'))
+	.then(datos=>{ 
+	 if(!isNaN(req.params.id) && datos.length > req.params.id){
+       const search = datos.filter( product => product.id === parseInt(req.params.id,10))
+       res.send(...search ) 
     }else{
-        res.send({error: 'producto no encontrado'})
+       res.send({error: 'producto no encontrado'})
     }
+ })
+
 })
 
  //c
  app.post("api/productos/guardar", (req, res)=>{
-   
-
+	let myfile = new Archivo ('./productosD8.txt');
+	let saveProduct = req.body;
+	myfile.guardarAsync(saveProduct);
+	return res.json({msg:"producto guardado"})
 })
 
 
 
 
  
-app.listen(port, ()=>{
-    console.log(`escuchando ${port} `)
+const server = app.listen(port, ()=>{
+    console.log(`servidor http escuchando en el puerto ${port} `)
 })
+server.on("error", error => console.log(`Error en el servidor ${error}`))
